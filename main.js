b780e0c35ff0d8fbb59485038913ed325191b5ec
Chung Hong <conconart.info@gmail.com>
2020-09-07 16:44:28 -0600

    Add functionality to letsCookButton handler

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var startingMenu = document.querySelector("form");
-// var chosenDish = document.querySelector("#result");
  var sideDishOption = document.querySelector("#side");
  var mainDishOption = document.querySelector("#main-dish");
  var dessertOption = document.querySelector("#dessert");
@@ -7,6 +6,7 @@ var startingMenu = document.querySelector("form");
 
 var cookPotImage = document.querySelector(".cookpot-image");
 var suggestions = document.querySelector(".suggestions");
+var suggestionsText = document.querySelector(".suggestions-text");
 
 var letsCookButton = document.querySelector(".lets-cook-button");
 var clearButton = document.querySelector(".clear-button");
@@ -16,7 +16,9 @@ var clearButton = document.querySelector(".clear-button");
 var chosenDish;
 
 //Event Listeners:
-letsCookButton.addEventListener("submit", letsCookButtonHandler);
+// letsCookButton.addEventListener("click", letsCookButtonHandler);
+window.addEventListener("load", showCookPot);
+letsCookButton.addEventListener("click", courseSelector);
 clearButton.addEventListener("click", clearButtonHandler);
 // addNewButton.addEventListener("click", addNewButtonHandler);
 
@@ -26,44 +28,28 @@ function getRandomIndex(array) {
 }
 
 function courseSelector() {
+  event.preventDefault();
+  hideCookPot();
   var foodMenu = document.getElementsByName("select-course");
   for (var i = 0; i < foodMenu.length; i++) {
     if(foodMenu[i].checked) {
       chosenDish = foodMenu[i].value;
-      // console.log(`You should make ${chosenDish} !`);
-      // ${chosenDish.getRandomIndex}
     }
   }
   if (chosenDish === "side") {
-    return `You should make ${sideDish[getRandomIndex(sideDish)]} `
+    suggestionsText.innerText = `${sideDish[getRandomIndex(sideDish)]}!`;
   } else if (chosenDish === "main-dish") {
-    return `You should make ${mainDish[getRandomIndex(mainDish)]}`
+    suggestionsText.innerText = `${mainDish[getRandomIndex(mainDish)]}!`;
   } else if (chosenDish === "dessert") {
-    return `You should make ${dessert[getRandomIndex(dessert)]}`
+    suggestionsText.innerText = `${dessert[getRandomIndex(dessert)]}!`;
   }
+  showClearButton();
+  showSuggestions();
 };
-//if "side" is chosen, random side is returned
-//if "main-dish" is chosen, random main dish is returned
-//if "dessert" is chosen, random dessert is returned
 //if "entire meal" is chosen, one random value for each category is returned (object)
 
 function addNewButtonHandler() {
   //when "addNewButton" is clicked, the main page is reset
-}
-
-function chooseSideDish() {
-  //randomizer
-  return getRandomIndex(sideDish[i]);
-}
-
-function chooseMainDish() {
-  //randomizer
-  return getRandomIndex(mainDish[i]);
-}
-
-function chooseDessert() {
-  //randomizer
-  return getRandomIndex(dessert[i]);
 }
 
 function chooseEntireMeal() {
@@ -74,30 +60,34 @@ function chooseEntireMeal() {
 }
 
 function hideCookPot() {
+  // event.preventDefault();
   //when let's cook button" is clicked, cookpot img disappears
   cookPotImage.classList.add("hidden");
 };
 
-function showCookPot() {
-  cookPotImage.classList.remove("hidden");
-}
+
 
 function showClearButton() {
   clearButton.classList.remove("hidden");
 }
 
 function showSuggestions() {
-  clearButton.classList.remove("hidden");
+  suggestions.classList.remove("hidden");
 }
+
 //Event Handlers:
+
+function showCookPot() {
+  cookPotImage.classList.remove("hidden");
+}
+
 function letsCookButtonHandler() {
-  // event.preventDefault();
-  hideCookpot();
+  event.preventDefault();
   courseSelector();
+  hideCookpot();
   showSuggestions();
   showClearButton();
   //when "let's cook button" is clicked,
-  //it has blue border and lighter color font
   //each option triggers another view page randomizer
   //"clear" button is visible
 };
